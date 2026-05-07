476e6665aefaf48fcb7a6c9845fc286641f4df0e
TommyH6211 <tommyhou1126ca@gmail.com>
2026-05-07 19:58:05 -0400

    Initial Spring Boot setup

---
+++ src/main/java/com/tommy/lostandfoundtommy/LostandfoundtommyApplication.java
@@ -0,0 +1,13 @@
+package com.tommy.lostandfoundtommy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LostandfoundtommyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LostandfoundtommyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tommy/lostandfoundtommy/LostandfoundtommyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tommy.lostandfoundtommy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LostandfoundtommyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
